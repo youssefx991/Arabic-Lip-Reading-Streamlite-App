5cc4ea78864e254ea1e4cb8adeff7520f864b156
Youssef Wagih <youssefwagih212@gmail.com>
2025-03-14 19:34:21 +0200

    download weights once

--- streamlitapp.py
+++ streamlitapp.py
@@ -10,10 +10,8 @@ import os
 
 def main():
     st.set_page_config(layout='wide')
-    if 'weights_downloaded' not in st.session_state:
+    if not os.path.exists("checkpoint.weights.h5"):
         download_weights()
-        st.session_state['weights_downloaded'] = True
-
     with st.sidebar:
         st.image('https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png')
         st.title('LipBuddy')
